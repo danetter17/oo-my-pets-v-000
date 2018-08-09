b6bdc47a15ddd10464f6339bdee864ea300be5a8
Daniel Etter <etter.dt@gmail.com>
2018-08-09 17:23:46 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,8 +61,6 @@ class Owner
   end
   
   def list_pets
-    #binding.pry
-
-   "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
 end
